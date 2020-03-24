65021f9ed011c2586e1e133486529b7662f95ad9
Daniel Sullivan <danielsullivan@Daniels-MacBook-Air.local>
2020-03-23 22:46:43 -0500

    Updates

---
+++ precache-manifest.aca32a6874076b03d695396b4d0b4544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f0a005b249183df278e2e826723f67",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "2520fe64cee21df8894b",
+    "url": "/MemoryGame/static/css/main.1e1dd4fa.chunk.css"
+  },
+  {
+    "revision": "2846038a885903b966c8",
+    "url": "/MemoryGame/static/js/2.d0a96f64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemoryGame/static/js/2.d0a96f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2520fe64cee21df8894b",
+    "url": "/MemoryGame/static/js/main.69179ea7.chunk.js"
+  },
+  {
+    "revision": "74ca52135554d9f64a97",
+    "url": "/MemoryGame/static/js/runtime-main.eb9d0161.js"
+  }
+]);
